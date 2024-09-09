64d52cdfe303e3dd64b429933ba702693081f140
Louiloui <162288706+Janinloui@users.noreply.github.com>
2024-09-09 15:37:39 +0200

    Update Counting walls.py

--- Counting walls.py
+++ Counting walls.py
@@ -1,5 +1,5 @@
 import ifcopenshell
-model = ifcopenshell.open('C:/Users/Lenovo/Desktop/CES_BLD_24_06_STR.ifc')
+model = ifcopenshell.open('C:/Users/Lenovo/Desktop/CES_BLD_24_06_STR.ifc') #change to your location
 
 walls = model.by_type('IfcWall')
 print(f"Walls in model: {len(walls)}")
