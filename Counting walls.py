c24ffec775728e78c2bc8e0a09983053f438220b
Louiloui <162288706+Janinloui@users.noreply.github.com>
2024-09-30 14:51:04 +0200

    Delete Counting walls.py

--- Counting walls.py
+++
@@ -1,5 +0,0 @@
-import ifcopenshell
-model = ifcopenshell.open('C:/Users/Lenovo/Desktop/CES_BLD_24_06_STR.ifc') #change to your location
-
-slab = model.by_type('IfcSlab')
-print(f"Slabs in model: {len(slab)}")
